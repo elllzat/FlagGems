ab4bf10b863eaf4e0350d433312aead061992c61
艾力扎提 <2841860141@qq.com>
2026-04-20 20:28:20 +0800

    local development version

---
+++ FlagGems-master/src/flag_gems/ops/silu.py
@@ -0,0 +1,54 @@
+import logging
+
+import torch
+import triton
+import triton.language as tl
+
+from flag_gems.utils import pointwise_dynamic
+from flag_gems.utils.triton_lang_extension import div_rn
+
+logger = logging.getLogger(__name__)
+
+_FALLBACK_KEYSET = torch._C.DispatchKeySet(
+    torch._C.DispatchKey.CompositeImplicitAutograd
+)
+
+
+@pointwise_dynamic(promotion_methods=[(0, "DEFAULT")])
+@triton.jit
+def silu_forward(x):
+    x_fp32 = x.to(tl.float32)
+    y = tl.fdiv(x_fp32, (1.0 + tl.exp(-x_fp32)))
+    return y
+
+
+@pointwise_dynamic(promotion_methods=[(0, "DEFAULT")])
+@triton.jit
+def silu_backward_kernel(x, dy):
+    dy_fp32 = dy.to(tl.float32)
+    x_fp32 = x.to(tl.float32)
+    sigma = div_rn(1.0, 1.0 + tl.exp(-x_fp32))
+    dx = dy_fp32 * sigma * (1.0 + x_fp32 * (1.0 - sigma))
+    return dx
+
+
+def silu(self):
+    logger.debug("GEMS SILU FORWARD")
+    output = silu_forward(self)
+    return output
+
+
+def silu_backward(grad_output, self):
+    logger.debug("GEMS SILU BACKWARD")
+    if torch.is_grad_enabled():
+        return torch.ops.aten.silu_backward.default.redispatch(
+            _FALLBACK_KEYSET, grad_output, self
+        )
+    grad_input = silu_backward_kernel(self, grad_output)
+    return grad_input
+
+
+def silu_(A):
+    logger.debug("GEMS SILU_ FORWARD")
+    out = silu_forward(A, out0=A)
+    return out
---
+++ FlagGems-master/src/flag_gems/runtime/__init__.py
@@ -0,0 +1,58 @@
+from . import backend, common, error
+from .backend.device import DeviceDetector
+from .configloader import ConfigLoader
+
+config_loader = ConfigLoader()
+device = DeviceDetector()
+
+"""
+The dependency order of the sub-directory is strict, and changing the order arbitrarily may cause errors.
+"""
+
+# torch_device_fn is like 'torch.cuda' object
+backend.set_torch_backend_device_fn(device.vendor_name)
+torch_device_fn = backend.gen_torch_device_object()
+
+# torch_backend_device is like 'torch.backend.cuda' object
+torch_backend_device = backend.get_torch_backend_device_fn()
+
+
+def get_tuned_config(op_name):
+    return config_loader.get_tuned_config(op_name)
+
+
+def get_heuristic_config(op_name):
+    return config_loader.get_heuristics_config(op_name)
+
+
+def replace_customized_ops(_globals):
+    event = backend.BackendArchEvent()
+    arch_specific_ops = event.get_arch_ops() if event.has_arch else None
+    extended_ops = backend.get_customized_ops(device.vendor_name)
+    if device.vendor != common.vendors.NVIDIA:
+        try:
+            for fn_name, fn in extended_ops:
+                _globals[fn_name] = fn
+        except RuntimeError as e:
+            error.customized_op_replace_error(e)
+    if arch_specific_ops:
+        try:
+            for fn_name, fn in arch_specific_ops:
+                _globals[fn_name] = fn
+        except RuntimeError as e:
+            error.customized_op_replace_error(e)
+
+
+def get_expand_config(op_name, yaml_path=None):
+    return config_loader.get_expand_config(op_name=op_name, yaml_path=yaml_path)
+
+
+def ops_get_configs(op_name, pre_hook=None, yaml_path=None):
+    return config_loader.ops_get_configs(
+        op_name=op_name,
+        pre_hook=pre_hook,
+        yaml_path=yaml_path,
+    )
+
+
+__all__ = ["*"]
