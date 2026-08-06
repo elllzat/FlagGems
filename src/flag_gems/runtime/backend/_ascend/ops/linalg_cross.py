da619fec8e1da54a226825560d679e91fbc830b9
elllzat <elllzat@users.noreply.github.com>
2026-08-06 20:39:19 +0800

    [KMCompiler]Improve linalg_cross Ascend precision

--- src/flag_gems/runtime/backend/_ascend/ops/linalg_cross.py
+++ src/flag_gems/runtime/backend/_ascend/ops/linalg_cross.py
@@ -70,9 +70,8 @@ def _linalg_cross_real_kernel(
     other_next = tl.gather(other_values, next_offsets, axis=0)
     other_last = tl.gather(other_values, last_offsets, axis=0)
 
-    element_ty = input_ptr.dtype.element_ty
-    product_next_last = (input_next * other_last).to(element_ty)
-    product_last_next = (input_last * other_next).to(element_ty)
+    product_next_last = input_next.to(tl.float32) * other_last.to(tl.float32)
+    product_last_next = input_last.to(tl.float32) * other_next.to(tl.float32)
     output_values = product_next_last - product_last_next
     tl.store(output_ptr + global_offsets, output_values, mask=mask)
 
@@ -273,13 +272,12 @@ def _linalg_cross_real_strided_kernel(
         other_ptr + other_base + 2 * OTHER_COMPONENT_STRIDE, mask=mask, other=0.0
     )
 
-    element_ty = input_ptr.dtype.element_ty
-    product_12 = (input_1 * other_2).to(element_ty)
-    product_21 = (input_2 * other_1).to(element_ty)
-    product_20 = (input_2 * other_0).to(element_ty)
-    product_02 = (input_0 * other_2).to(element_ty)
-    product_01 = (input_0 * other_1).to(element_ty)
-    product_10 = (input_1 * other_0).to(element_ty)
+    product_12 = input_1.to(tl.float32) * other_2.to(tl.float32)
+    product_21 = input_2.to(tl.float32) * other_1.to(tl.float32)
+    product_20 = input_2.to(tl.float32) * other_0.to(tl.float32)
+    product_02 = input_0.to(tl.float32) * other_2.to(tl.float32)
+    product_01 = input_0.to(tl.float32) * other_1.to(tl.float32)
+    product_10 = input_1.to(tl.float32) * other_0.to(tl.float32)
     tl.store(output_ptr + output_base, product_12 - product_21, mask=mask)
     tl.store(
         output_ptr + output_base + OUTPUT_COMPONENT_STRIDE,
@@ -468,9 +466,8 @@ def _linalg_cross_real_lastdim_broadcast_kernel(
     other_next = tl.gather(other_values, next_offsets, axis=0)
     other_last = tl.gather(other_values, last_offsets, axis=0)
 
-    element_ty = input_ptr.dtype.element_ty
-    product_next_last = (input_next * other_last).to(element_ty)
-    product_last_next = (input_last * other_next).to(element_ty)
+    product_next_last = input_next.to(tl.float32) * other_last.to(tl.float32)
+    product_last_next = input_last.to(tl.float32) * other_next.to(tl.float32)
     output_values = product_next_last - product_last_next
     tl.store(output_ptr + global_offsets, output_values, mask=mask)
 
@@ -590,13 +587,12 @@ def _linalg_cross_real_dim1_3d_kernel(
     other_1 = tl.load(other_ptr + other_base + INNER_SIZE, mask=mask, other=0.0)
     other_2 = tl.load(other_ptr + other_base + 2 * INNER_SIZE, mask=mask, other=0.0)
 
-    element_ty = input_ptr.dtype.element_ty
-    product_12 = (input_1 * other_2).to(element_ty)
-    product_21 = (input_2 * other_1).to(element_ty)
-    product_20 = (input_2 * other_0).to(element_ty)
-    product_02 = (input_0 * other_2).to(element_ty)
-    product_01 = (input_0 * other_1).to(element_ty)
-    product_10 = (input_1 * other_0).to(element_ty)
+    product_12 = input_1.to(tl.float32) * other_2.to(tl.float32)
+    product_21 = input_2.to(tl.float32) * other_1.to(tl.float32)
+    product_20 = input_2.to(tl.float32) * other_0.to(tl.float32)
+    product_02 = input_0.to(tl.float32) * other_2.to(tl.float32)
+    product_01 = input_0.to(tl.float32) * other_1.to(tl.float32)
+    product_10 = input_1.to(tl.float32) * other_0.to(tl.float32)
     tl.store(output_ptr + output_base, product_12 - product_21, mask=mask)
     tl.store(
         output_ptr + output_base + INNER_SIZE,
@@ -654,13 +650,12 @@ def _linalg_cross_real_dim1_small_inner_kernel(
 
     output_base = batches[:, None] * (3 * INNER_SIZE) + inner[None, :]
     output_mask = batch_mask & inner_mask
-    element_ty = input_ptr.dtype.element_ty
-    product_12 = (input_1 * other_2).to(element_ty)
-    product_21 = (input_2 * other_1).to(element_ty)
-    product_20 = (input_2 * other_0).to(element_ty)
-    product_02 = (input_0 * other_2).to(element_ty)
-    product_01 = (input_0 * other_1).to(element_ty)
-    product_10 = (input_1 * other_0).to(element_ty)
+    product_12 = input_1.to(tl.float32) * other_2.to(tl.float32)
+    product_21 = input_2.to(tl.float32) * other_1.to(tl.float32)
+    product_20 = input_2.to(tl.float32) * other_0.to(tl.float32)
+    product_02 = input_0.to(tl.float32) * other_2.to(tl.float32)
+    product_01 = input_0.to(tl.float32) * other_1.to(tl.float32)
+    product_10 = input_1.to(tl.float32) * other_0.to(tl.float32)
     tl.store(output_ptr + output_base, product_12 - product_21, mask=output_mask)
     tl.store(
         output_ptr + output_base + INNER_SIZE,
